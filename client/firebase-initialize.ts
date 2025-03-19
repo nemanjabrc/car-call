e7cd5a5bf4425552a40009a47c5abc581b2b69ef
nemanjabrc <154009085+nemanjabrc@users.noreply.github.com>
2025-03-19 12:50:25 +0100

    Manja izmjena na frontendu.

--- client/firebase-initialize.ts
+++ client/firebase-initialize.ts
@@ -3,20 +3,10 @@ import { initializeApp } from "firebase/app";
 import { getMessaging } from 'firebase/messaging';
 import { onMessage } from "firebase/messaging";
 import { toast } from "react-toastify";
+import { firebaseConfig } from "./firebase-config";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyCFym9u0T0-HzGRitV__HozXfZ35MQJLCE",
-  authDomain: "car-call-29ebb.firebaseapp.com",
-  projectId: "car-call-29ebb",
-  storageBucket: "car-call-29ebb.firebasestorage.app",
-  messagingSenderId: "712326170696",
-  appId: "1:712326170696:web:19ff4886e7b9e68845707d",
-  measurementId: "G-7BNEF66SMV"
-};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
